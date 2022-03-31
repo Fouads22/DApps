a29805e7d4f9e0002bff663fe584d0453034eadb
Fouads22 <90304101+Fouads22@users.noreply.github.com>
2022-03-31 14:35:54 +0300

    Update HelloResource.java

--- Project04/HelloResource.java
+++ Project04/HelloResource.java
@@ -1,15 +1,9 @@
 package com.example.my_ejb;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ejb.Stateless;
 
-@Path("/hello-world")
 @Stateless
-public class HelloResource {
-    @GET
-    @Produces("text/plain")
+public class HelloResource implements Hello {
     public String hello() {
         return "Hello, Fouad Saba";
     }
