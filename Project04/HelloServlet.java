a2ec6337caea3837b151e8eb23b24b93fd549bb1
Fouads22 <90304101+Fouads22@users.noreply.github.com>
2022-03-31 14:36:24 +0300

    Update HelloServlet.java

--- Project04/HelloServlet.java
+++ Project04/HelloServlet.java
@@ -1,25 +1,21 @@
 package com.example.my_ejb;
 
 import java.io.*;
+import javax.ejb.EJB;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-    private HelloResource hr = new HelloResource();
-
-    public void init() {
-        message = hr.hello();
-    }
-
+    @EJB
+    Hello hr =new HelloResource();
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + hr.hello() + "</h1>");
         out.println("</body></html>");
     }
 
